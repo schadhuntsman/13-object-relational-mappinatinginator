69f06fc0de53edbfe52fb04f44ec821014b59a33
schadhuntsman <104479454+schadhuntsman@users.noreply.github.com>
2022-07-22 17:06:03 -0700

    updated table for category

--- main/models/Category.js
+++ main/models/Category.js
@@ -1,18 +1,64 @@
 const { Model, DataTypes } = require('sequelize');
+const { DataTypes } = require('sequelize/types/index.js');
+const { DataTypes } = require('sequelize/types/index.js');
 
 const sequelize = require('../config/connection.js');
 
+// create our User model
 class Category extends Model {}
 
+// define table columns and configuration
 Category.init(
   {
-    // define columns
+    // define an id column
+    id: {
+      type: DataTypes.INTEGER,
+      // this is the equivalent of SQL's `NOT NULL` option
+      allowNull: false,
+      // instruct that this is the Primary Key
+      primaryKey: true,
+     // turn on auto increment
+     autoIncrement: true
+    
+    },
+    
+    // TABLE COLUMN DEFINITIONS GO HERE
+  }, //define shirts
+  Shirts: {
+    type: DataTypes.STRING,
+    allowNull: false,
   },
+  //define shorts
+  Shorts: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },//define music
+  Music: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  //define hats
+  Hats: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },//define Shoes
+  Shoes: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
   {
+    // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
+
+    // pass in our imported sequelize connection (the direct connection to our database)
     sequelize,
+     // don't automatically create createdAt/updatedAt timestamp fields
     timestamps: false,
+    // don't pluralize name of database table
     freezeTableName: true,
+     // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
     underscored: true,
+    // make it so our model name stays lowercase in the database
     modelName: 'category',
   }
 );
